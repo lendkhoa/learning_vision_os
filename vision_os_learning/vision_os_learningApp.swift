02393383ce2c579847550791220b0f70c0b61cbf
Khoa Le <khoale.inbox@gmail.com>
2023-07-03 23:56:48 -0500

    Initial Commit

---
+++ vision_os_learning/vision_os_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  vision_os_learningApp.swift
+//  vision_os_learning
+//
+//  Created by Khoa Le on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct vision_os_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
